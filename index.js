7b25e2e44db935a6ed2652af1e18e268af6c15d4
Gediminas <gediminasgn@gmail.com>
2017-03-06 15:27:48 +0000

    Export structure update

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 /* ATOMIC components */
 
-import ActionButton from './src/sdk/ActionButton/ActionButton'
-import InputCheckbox from './src/sdk/FormElements/InputCheckbox'
-import InputRadio from './src/sdk/FormElements/InputRadio'
-import InputText from './src/sdk/FormElements/InputText'
-import OptionsRow from './src/sdk/FormElementsCustom/OptionsRow'
-import HorizontalNav from './src/sdk/HorizontalNav/HorizontalNav'
-import VerticalNav from './src/sdk/VerticalNav/VerticalNav'
-import LayoutVertical from './src/sdk/Layouts/LayoutVertical'
-import LayoutWithSidebar from './src/sdk/Layouts/LayoutWithSidebar'
-import Fonts from './src/sdk/Typography/Fonts'
+import { ActionButton } from './src/sdk/ActionButton/ActionButton'
+import { InputCheckbox } from './src/sdk/FormElements/InputCheckbox'
+import { InputRadio } from './src/sdk/FormElements/InputRadio'
+import { InputText } from './src/sdk/FormElements/InputText'
+import { OptionsRow } from './src/sdk/FormElementsCustom/OptionsRow'
+import { HorizontalNav } from './src/sdk/HorizontalNav/HorizontalNav'
+import { VerticalNav } from './src/sdk/VerticalNav/VerticalNav'
+import { LayoutVertical } from './src/sdk/Layouts/LayoutVertical'
+import { LayoutWithSidebar } from './src/sdk/Layouts/LayoutWithSidebar'
+import { Fonts } from './src/sdk/Typography/Fonts'
 
 export ActionButton
 
